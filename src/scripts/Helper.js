8bf47c12fca11d6a22a9c1034697842fe6567f40
vadim-p <booze.mill@gmail.com>
2016-10-20 19:18:44 +0300

    jsdoc: Helper

--- src/scripts/Helper.js
+++ src/scripts/Helper.js
@@ -1,11 +1,30 @@
+/**
+ * Helper class containing methods called from several places
+ * @constructor
+ */
 function Helper(){
 
 }
 
+/**
+ * Checks if HTTP status is between 200 and 400
+ * @param status
+ * @returns {boolean}
+ *
+ * @since 1.0.0
+ */
 Helper.prototype.isSuccessHTTPStatus = function(status){
   return status >= 200 && status < 400;
 };
 
+/**
+ * Returns structured answer from XMLHttpRequest
+ * @param {*} data
+ * @param {object} XMLHttpRequest
+ * @returns {{data: (*|null), status: number, statusText: string}}
+ *
+ * @since 1.0.0
+ */
 Helper.prototype.getTransformedResponse = function(data, XMLHttpRequest){
   return {
     data      : data || null,
@@ -13,14 +32,36 @@ Helper.prototype.getTransformedResponse = function(data, XMLHttpRequest){
     statusText: XMLHttpRequest ? XMLHttpRequest.statusText : ''
   };
 };
+
+/**
+ * Checks if argument is array
+ * @param any
+ * @returns {boolean}
+ *
+ * @since 1.0.0
+ */
 Helper.prototype.isArray = function(any){
-  return typeof any === 'object' && any instanceof Array && Object.prototype.toString.call( any ) === '[object Array]';
+  return typeof any === 'object' && any instanceof Array && Object.prototype.toString.call(any) === '[object Array]';
 };
 
+/**
+ * Checks if argument is object
+ * @param any
+ * @returns {boolean}
+ *
+ * @since 1.0.0
+ */
 Helper.prototype.isObject = function(any){
-  return any === Object(any) && Object.prototype.toString.call( any ) !== '[object Array]';
+  return any === Object(any) && Object.prototype.toString.call(any) !== '[object Array]';
 };
 
+/**
+ * Removes {null} and {undefined} from {object}
+ * @param {object} object
+ * @returns {*}
+ *
+ * @since 1.0.0
+ */
 Helper.prototype.cleanObject = function(object){
   if(!this.isObject(object)){
     return object;
