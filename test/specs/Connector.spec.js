7d96fcb8ed1a3b7e0511756ea742f4a2d6e380dc
vadim-p <booze.mill@gmail.com>
2016-11-28 18:46:41 +0200

    tests(Connector): refactor to use only fake server

--- test/specs/Connector.spec.js
+++ test/specs/Connector.spec.js
@@ -1,13 +1,11 @@
 describe('Connector class', function(){
-  var apiPath = 'https://lpw-public-api.herokuapp.com';
-  // var apiPath = 'https://staging-lpw-public-api.herokuapp.com';
+  var propertyId = 70979;
 
   describe('#readPropertyById()', function(){
     var connector,
-        // server,
         xhr,
         requests,
-        singlePropId = 70979;
+        singlePropId = propertyId;
 
     var fakeResponseBody = {
       id  : singlePropId,
@@ -17,56 +15,36 @@ describe('Connector class', function(){
 
     beforeEach(function(){
       connector = new Connector('etd4xUyDUMsa47sQBwNB');
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function(req){ requests.push(req); };
+    });
+
+    afterEach(function(){
+      xhr.restore();
     });
 
     it('should exist', function(){
       should.exist(connector.readPropertyById);
     });
 
-    describe('isolated', function(){
-      beforeEach(function(){
-        xhr = sinon.useFakeXMLHttpRequest();
-        requests = [];
-        xhr.onCreate = function(req){ requests.push(req); };
-      });
-
-      afterEach(function(){
-        xhr.restore();
-      });
-
-      it('should make request and return XMLHttpRequest with response', function(){
-        var callback = sinon.spy();
-        connector.readPropertyById(singlePropId, 'en', callback);
-        requests[0].respond(
-          200,
-          {"Content-Type": "application/json"},
-          JSON.stringify(fakeResponseBody)
-        );
-        callback.calledOnce.should.be.true;
-        callback.args.length.should.be.equal(1);
-        callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBody));
-      });
-    });
-
-    describe('live', function(){
-      it('should make request and return XMLHttpRequest with response', function(done){
-        var myCallback = function(request){
-          var response = JSON.parse(request.response);
-          request.should.exist;
-          request.status.should.be.equal(200);
-          response.should.have.ownProperty('id');
-          response.should.have.ownProperty('code');
-          response.id.should.be.equal(singlePropId);
-          done();
-        };
-        connector.readPropertyById(singlePropId, 'en', myCallback);
-      });
+    it('should make GET request and call callback with XMLHttpRequest as first argument', function(){
+      var callback = sinon.spy();
+      connector.readPropertyById(singlePropId, 'en', callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify(fakeResponseBody)
+      );
+      callback.calledOnce.should.be.true;
+      callback.args.length.should.be.equal(1);
+      callback.args[0][0].method.should.be.equal('GET');
+      callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBody));
     });
   });
 
   describe('#readProperties()', function(){
     var connector,
-        server,
         xhr,
         requests,
         options          = {},
@@ -88,51 +66,32 @@ describe('Connector class', function(){
 
     beforeEach(function(){
       connector = new Connector('etd4xUyDUMsa47sQBwNB');
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function(req){ requests.push(req); };
+    });
+
+    afterEach(function(){
+      xhr.restore();
     });
 
     it('should exist', function(){
       should.exist(connector.readProperties);
     });
 
-    describe('isolated', function(){
-      beforeEach(function(){
-        xhr = sinon.useFakeXMLHttpRequest();
-        requests = [];
-        xhr.onCreate = function(req){ requests.push(req); };
-      });
-
-      afterEach(function(){
-        xhr.restore();
-      });
-
-      it('should make request and return XMLHttpRequest with response', function(){
-        var callback = sinon.spy();
-        connector.readProperties(options, callback, function(){});
-        requests[0].respond(
-          200,
-          {"Content-Type": "application/json"},
-          JSON.stringify(fakeResponseBody)
-        );
-        callback.calledOnce.should.be.true;
-        callback.args.length.should.be.equal(1);
-        callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBody));
-      });
+    it('should make GET request and call callback with XMLHttpRequest as first argument', function(){
+      var callback = sinon.spy();
+      connector.readProperties(options, callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify(fakeResponseBody)
+      );
+      callback.calledOnce.should.be.true;
+      callback.args.length.should.be.equal(1);
+      callback.args[0][0].method.should.be.equal('GET');
+      callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBody));
     });
-
-    describe('live', function(){
-      it('should make request and return XMLHttpRequest with response', function(done){
-        this.timeout(3000);
-        var myCallback = function(request){
-          var response = JSON.parse(request.response);
-          request.should.exist;
-          request.status.should.be.equal(200);
-          response.should.have.ownProperty('property_objects');
-          response.property_objects.length.should.be.above(1);
-          done();
-        };
-        connector.readProperties(options, myCallback, function(){});
-      });
-    })
   });
 
   describe('#readCurrencies()', function(){
@@ -146,53 +105,87 @@ describe('Connector class', function(){
 
     beforeEach(function(){
       connector = new Connector('etd4xUyDUMsa47sQBwNB');
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function(req){ requests.push(req); };
+    });
+
+    afterEach(function(){
+      xhr.restore();
     });
 
     it('should exist', function(){
       should.exist(connector.readCurrencies);
     });
 
-    describe('isolated', function(){
-      beforeEach(function(){
-        xhr = sinon.useFakeXMLHttpRequest();
-        requests = [];
-        xhr.onCreate = function(req){ requests.push(req); };
-      });
+    it('should make GET request and call callback with XMLHttpRequest as first argument', function(){
+      var callback = sinon.spy();
+      connector.readCurrencies(callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify(fakeResponseBody)
+      );
+      callback.calledOnce.should.be.true;
+      callback.args.length.should.be.equal(1);
+      callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBody));
+      callback.args[0][0].method.should.be.equal('GET');
+      callback.args[0][0].url.should.be.equal(connector.apiPath + '/currencies');
+    });
+  });
 
-      afterEach(function(){
-        xhr.restore();
-      });
+  describe('#readPDF()', function(){
+    var connector,
+        xhr,
+        requests,
+        fakeResponseBodySuccess = {
+          pdf_path: 'PDF_PATH'
+        },
+        locale = 'en';
 
-      it('should make GET request to /currencies and return XMLHttpRequest with response', function(){
-        var callback = sinon.spy();
-        connector.readCurrencies(callback);
-        requests[0].respond(
-          200,
-          {"Content-Type": "application/json"},
-          JSON.stringify(fakeResponseBody)
-        );
-        callback.calledOnce.should.be.true;
-        callback.args.length.should.be.equal(1);
-        callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBody));
-        callback.args[0][0].method.should.be.equal('GET');
-        callback.args[0][0].url.should.be.equal(apiPath + '/currencies');
-      });
+
+    beforeEach(function(){
+      connector = new Connector('etd4xUyDUMsa47sQBwNB');
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function(req){ requests.push(req); };
     });
 
-    describe('live', function(){
-      it('should make request and call userCallback with answer as first argument', function(done){
-        this.timeout(3000);
-        var myCallback = function(request){
-          console.log(request);
-          var response = JSON.parse(request.response);
-          request.should.exist;
-          request.status.should.be.equal(200);
-          response.should.have.ownProperty('currencies');
-          response.currencies.length.should.be.above(0);
-          done();
-        };
-        connector.readCurrencies(myCallback);
-      });
-    })
-  })
+    afterEach(function(){
+      xhr.restore();
+    });
+
+    it('should exist', function(){
+      should.exist(connector.readPDF);
+    });
+
+    it('should make GET request and call callback with XMLHttpRequest as first argument', function(){
+      var callback = sinon.spy();
+      connector.readPDF(propertyId, false, locale, callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify(fakeResponseBodySuccess)
+      );
+      callback.calledOnce.should.be.true;
+      callback.args.length.should.be.equal(1);
+      callback.args[0][0].responseText.should.be.eql(JSON.stringify(fakeResponseBodySuccess));
+      callback.args[0][0].method.should.be.equal('GET');
+      callback.args[0][0].url.should.be.equal(connector.apiPath + '/' + locale +  '/pdf/' + propertyId);
+    });
+
+    it('should add `for_rent` query parameter if present', function(){
+      var callback = sinon.spy();
+      connector.readPDF(propertyId, true, locale, callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify(fakeResponseBodySuccess)
+      );
+      callback.calledOnce.should.be.true;
+      callback.args.length.should.be.equal(1);
+      console.log(callback.args[0][0].url);
+      /\?for_rent=true/.test(callback.args[0][0].url).should.be.true;
+    });
+  });
 });
