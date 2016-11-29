853fd4284529e0ed7552561155c7079ada3b0427
vadim-p <booze.mill@gmail.com>
2016-11-29 12:33:54 +0200

    tests(LPW): replaced all live requests by fake sinonjs XHR

--- test/specs/LPW.spec.js
+++ test/specs/LPW.spec.js
@@ -1,93 +1,144 @@
 describe('LPW class', function(){
-  describe('#getProperties()', function(){
-    var lpw;
+  var propertyId = 70979,
+      lpw,
+      xhr,
+      requests;
 
+  describe('#getProperties()', function(){
+    var fakeResponse            = {
+          property_objects: [],
+          offmarket       : 10,
+          total           : 20
+        },
+        fakeResponseStringified = JSON.stringify(fakeResponse);
     beforeEach(function(){
       lpw = new LPW('etd4xUyDUMsa47sQBwNB', {debugEnabled: true});
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function (xhr) {
+        requests.push(xhr);
+      };
+    });
+
+    afterEach(function(){
+      xhr.restore();
     });
 
     it('should exist', function(){
       lpw.getProperties.should.exist;
     });
 
-    it('should return undefined and log a message once if no callback specified', function(){
-      should.not.exist(lpw.getProperties());
+    it('should throw TypeError if no userCallback or userCallback is not a function', function(){
+      should.Throw(function(){lpw.getProperties()}, TypeError);
     });
 
-    it('should return undefined if callback is not a function', function(){
-      should.not.exist(lpw.getProperties({}));
-    });
-
-    it('should log a message once if callback is not a function', function(){
-      var myLog = sinon.spy(console, 'log');
-      lpw.getProperties();
-      myLog.should.be.called;
-      myLog.callCount.should.be.equal(1);
-      console.log.restore();
-    });
-
-    it('should return object as callback argument', function(done){
-      lpw.getProperties({}, function(answer){
-        answer.should.be.an('object');
-        done();
-      });
-    });
-
-    it('should return object with necessary data', function(done){
-      lpw.getProperties({}, function(answer){
-        answer.should.be.an('object');
-        answer.should.have.property('data');
-        answer.should.have.property('status');
-        answer.should.have.property('statusText');
-        answer.should.have.deep.property('data.property_objects');
-        done();
-      });
+    it('should call userCallback with answer as first argument', function(){
+      var callback = sinon.spy();
+      lpw.getProperties({}, callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        fakeResponseStringified
+      );
+      callback.calledOnce.should.be.true;
+      var answer = callback.args[0][0];
+      answer.data.should.exist;
+      answer.data.should.be.an('object');
+      answer.data.should.have.ownProperty('property_objects');
+      answer.data.should.have.ownProperty('offmarket');
+      answer.data.should.have.ownProperty('total');
+      answer.status.should.be.a('number');
+      answer.statusText.should.be.a('string');
     });
   });
 
   describe('#getPropertyById()', function(){
-    var lpw,
-        propertyId = 70979;
+    var fakeResponse            = {
+          id: propertyId
+        },
+        fakeResponseStringified = JSON.stringify(fakeResponse);
 
     beforeEach(function(){
-      lpw = new LPW('etd4xUyDUMsa47sQBwNB');
+      lpw = new LPW('etd4xUyDUMsa47sQBwNB', {debugEnabled: true});
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function (xhr) {
+        requests.push(xhr);
+      };
+    });
+
+    afterEach(function(){
+      xhr.restore();
     });
 
     it('should exist', function(){
       should.exist(lpw.getPropertyById);
     });
 
-    it('should accept id and callback and call it with structured data', function(done){
-      lpw.getPropertyById(70979, {}, function(answer){
-        answer.data.should.exist;
-        answer.status.should.exist;
-        answer.statusText.should.exist;
-        answer.should.be.an('object');
-        answer.data.id.should.be.equal(propertyId);
-        done();
-      });
+    it('should throw TypeError if id is not a number', function(){
+      should.Throw(function(){lpw.getPropertyById('a', {}, function(){})}, TypeError);
+    });
+
+    it('should throw TypeError if callback is not a function', function(){
+      should.Throw(function(){lpw.getPropertyById(propertyId, {}, {})}, TypeError);
+    });
+
+    it('should call userCallback with answer as first argument', function(){
+      var callback = sinon.spy();
+      lpw.getPropertyById(propertyId, {}, callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        fakeResponseStringified
+      );
+      callback.calledOnce.should.be.true;
+      var answer = callback.args[0][0];
+      answer.data.should.exist;
+      answer.data.should.be.an('object');
+      answer.data.should.have.ownProperty('id');
+      answer.status.should.be.a('number');
+      answer.statusText.should.be.a('string');
     });
   });
 
   describe('#getCurrencies()', function(){
-    var lpw;
+    var fakeResponse            = {
+          currencies: []
+        },
+        fakeResponseStringified = JSON.stringify(fakeResponse);
 
     beforeEach(function(){
       lpw = new LPW('etd4xUyDUMsa47sQBwNB');
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function (xhr) {
+        requests.push(xhr);
+      };
+    });
+
+    afterEach(function(){
+      xhr.restore();
     });
 
     it('should exist', function(){
       should.exist(lpw.getCurrencies);
     });
 
-    it('should accept callback and call it with structured data', function(done){
-      lpw.getCurrencies(function(answer){
-        answer.data.should.exist;
-        answer.status.should.exist;
-        answer.statusText.should.exist;
-        answer.data.currencies.should.exist;
-        done();
-      });
+    it('should call userCallback with answer as first argument', function(){
+      var callback = sinon.spy();
+      lpw.getCurrencies(callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        fakeResponseStringified
+      );
+      callback.calledOnce.should.be.true;
+      var answer = callback.args[0][0];
+      answer.data.should.exist;
+      answer.data.should.be.an('object');
+      answer.data.should.have.ownProperty('currencies');
+      answer.status.should.be.a('number');
+      answer.statusText.should.be.a('string');
     });
   });
 
@@ -105,6 +156,47 @@ describe('LPW class', function(){
     it('should set locale', function(){
       lpw.setLocale('ru');
       lpw.locale.should.be.equal('ru');
+    });
+  });
+
+  describe('#getPDF()', function(){
+    var fakeResponse            = {
+          pdf_path: 'PDF_PATH'
+        },
+        fakeResponseStringified = JSON.stringify(fakeResponse);
+
+    beforeEach(function(){
+      lpw = new LPW('etd4xUyDUMsa47sQBwNB');
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = function (xhr) {
+        requests.push(xhr);
+      };
+    });
+
+    afterEach(function(){
+      xhr.restore();
+    });
+
+    it('should exist', function(){
+      should.exist(lpw.getPDF);
+    });
+
+    it('should call userCallback with answer as first argument', function(){
+      var callback = sinon.spy();
+      lpw.getPDF(propertyId, {}, callback);
+      requests[0].respond(
+        200,
+        {"Content-Type": "application/json"},
+        fakeResponseStringified
+      );
+      callback.calledOnce.should.be.true;
+      var answer = callback.args[0][0];
+      answer.data.should.exist;
+      answer.data.should.be.an('object');
+      answer.data.should.have.ownProperty('pdf_path');
+      answer.status.should.be.equal(200);
+      answer.statusText.should.be.equal('OK');
     });
   });
 });
